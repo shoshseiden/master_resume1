04ccbcbc7089f104861f64c7bbce360d6d0afb41
Shosh Seiden <shosh.j.seiden@gmail.com>
2018-01-21 18:18:05 -0700

    Created dropdown list for state choice

--- master_resume1/forms.py
+++ master_resume1/forms.py
@@ -1,13 +1,67 @@
 from django import forms
 import account.forms
 
+# State Choices
+STATE_CHOICES = (
+    ('Alabama', 'AL'),
+    ('Alaska', 'AK'),
+    ('Arizona', 'AZ'),
+    ('Arkansas', 'AR'),
+    ('California', 'CA'),
+    ('Colorado', 'CO'),
+    ('Connecticut', 'CT'),
+    ('Delaware', 'DE'),
+    ('Florida', 'FL'),
+    ('Georgia', 'GA'),
+    ('Hawaii', 'HI'),
+    ('Idaho', 'ID'),
+    ('Illinois', 'IL'),
+    ('Indiana', 'IN'),
+    ('Iowa', 'IA'),
+    ('Kansas', 'KS'),
+    ('Kentucky', 'KY'),
+    ('Louisiana', 'LA'),
+    ('Maine', 'ME'),
+    ('Maryland', 'MD'),
+    ('Massachusetts', 'MA'),
+    ('Michigan', 'MI'),
+    ('Minnesota', 'MN'),
+    ('Mississippi', 'MS'),
+    ('Missouri', 'MO'),
+    ('Montana', 'MT'),
+    ('Nebraska', 'NE'),
+    ('Nevada', 'NV'),
+    ('New Hampshire', 'NH'),
+    ('New Jersey', 'NJ'),
+    ('New Mexico', 'NM'),
+    ('New York', 'NY'),
+    ('North Carolina', 'NC'),
+    ('North Dakota', 'ND'),
+    ('Ohio', 'OH'),
+    ('Oklahoma', 'OK'),
+    ('Oregon', 'OR'),
+    ('Pennsylvania', 'PA'),
+    ('Rhode Island', 'RI'),
+    ('South Carolina', 'SC'),
+    ('South Dakota', 'SD'),
+    ('Tennessee', 'TN'),
+    ('Texas', 'TX'),
+    ('Utah', 'UT'),
+    ('Vermont', 'VT'),
+    ('Virginia', 'VA'),
+    ('Washington', 'WA'),
+    ('West Virginia', 'WV'),
+    ('Wisconsin', 'WI'),
+    ('Wyoming', 'WY'),
+)
+
 class SignupForm(account.forms.SignupForm):
     first_name = forms.CharField()
     last_name = forms.CharField()
     phone = forms.CharField()
     street_address = forms.CharField()
     city = forms.CharField()
-    state = forms.CharField()
+    state = forms.CharField(widget=forms.Select(choices=STATE_CHOICES))
     zip_code = forms.IntegerField()
 
     def __init__(self, *args, **kwargs):
