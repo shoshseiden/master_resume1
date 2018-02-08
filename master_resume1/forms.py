6b65e1c0e1bbd33aaff058e251ec0dcee386236b
Shosh Seiden <shosh.j.seiden@gmail.com>
2018-02-07 21:05:37 -0700

    created separate file for states list

--- master_resume1/forms.py
+++ master_resume1/forms.py
@@ -1,59 +1,8 @@
 from django import forms
-import account.forms
 
-# State Choices
-STATE_CHOICES = (
-    ('Alabama', 'AL'),
-    ('Alaska', 'AK'),
-    ('Arizona', 'AZ'),
-    ('Arkansas', 'AR'),
-    ('California', 'CA'),
-    ('Colorado', 'CO'),
-    ('Connecticut', 'CT'),
-    ('Delaware', 'DE'),
-    ('Florida', 'FL'),
-    ('Georgia', 'GA'),
-    ('Hawaii', 'HI'),
-    ('Idaho', 'ID'),
-    ('Illinois', 'IL'),
-    ('Indiana', 'IN'),
-    ('Iowa', 'IA'),
-    ('Kansas', 'KS'),
-    ('Kentucky', 'KY'),
-    ('Louisiana', 'LA'),
-    ('Maine', 'ME'),
-    ('Maryland', 'MD'),
-    ('Massachusetts', 'MA'),
-    ('Michigan', 'MI'),
-    ('Minnesota', 'MN'),
-    ('Mississippi', 'MS'),
-    ('Missouri', 'MO'),
-    ('Montana', 'MT'),
-    ('Nebraska', 'NE'),
-    ('Nevada', 'NV'),
-    ('New Hampshire', 'NH'),
-    ('New Jersey', 'NJ'),
-    ('New Mexico', 'NM'),
-    ('New York', 'NY'),
-    ('North Carolina', 'NC'),
-    ('North Dakota', 'ND'),
-    ('Ohio', 'OH'),
-    ('Oklahoma', 'OK'),
-    ('Oregon', 'OR'),
-    ('Pennsylvania', 'PA'),
-    ('Rhode Island', 'RI'),
-    ('South Carolina', 'SC'),
-    ('South Dakota', 'SD'),
-    ('Tennessee', 'TN'),
-    ('Texas', 'TX'),
-    ('Utah', 'UT'),
-    ('Vermont', 'VT'),
-    ('Virginia', 'VA'),
-    ('Washington', 'WA'),
-    ('West Virginia', 'WV'),
-    ('Wisconsin', 'WI'),
-    ('Wyoming', 'WY'),
-)
+from .states import States
+
+import account.forms
 
 
 class SignupForm(account.forms.SignupForm):
@@ -62,7 +11,7 @@ class SignupForm(account.forms.SignupForm):
     phone = forms.CharField()
     street_address = forms.CharField()
     city = forms.CharField()
-    state = forms.CharField(widget=forms.Select(choices=STATE_CHOICES))
+    state = forms.CharField(widget=forms.Select(choices=States.STATE_CHOICES))
     zip_code = forms.IntegerField()
 
     def __init__(self, *args, **kwargs):
